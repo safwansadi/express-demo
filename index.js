ba9a18e84592e14c992de50fa4ddcc24b7116698
safwansadi <safwaniftekher@gmail.com>
2021-03-16 16:03:34 +0600

    created first web server

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.get("/api/courses", (req, res) => {
+  res.send([1, 2, 3, 4, 4, 5, 7, 8, 78]);
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}...`);
+});
