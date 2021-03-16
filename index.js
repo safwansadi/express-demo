03fdc888baa4f536151be09cad934d7ab45b25b0
safwansadi <safwaniftekher@gmail.com>
2021-03-16 16:16:36 +0600

    routing params

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get("/api/courses", (req, res) => {
   res.send([1, 2, 3, 4, 4, 5, 7, 8, 78]);
 });
 
+app.get("/api/posts/:year/:month", (req, res) => {
+  res.send(req.params); //route parameters
+});
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
