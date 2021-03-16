1cd4010cf61fd9b922c1e37f0288d30bae5d76b7
safwansadi <safwaniftekher@gmail.com>
2021-03-16 16:54:05 +0600

    find course from array by routing parameter

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 const express = require("express");
 const app = express();
 
+const courses = [
+  { id: 1, name: "course1" },
+  { id: 2, name: "course2" },
+  { id: 3, name: "course3" },
+];
 app.get("/", (req, res) => {
   res.send("hello world");
 });
 
 app.get("/api/courses", (req, res) => {
-  res.send([1, 2, 3, 4, 4, 5, 7, 8, 78]);
+  res.send(courses);
 });
 
-app.get("/api/posts/:year/:month", (req, res) => {
-  res.send(req.params); //route parameters
+app.get("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) res.status(404).send("there is no course with given id ");
+  res.send(course);
 });
+
+/*app.get("/api/posts/:year/:month", (req, res) => {
+  res.send(req.params); //route parameters
+});*/
 
 const port = process.env.PORT || 3000;
 
